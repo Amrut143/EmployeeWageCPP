f3db946c3b7964899c80d480bf912f43b3219aff
Amrut <amrutpanda7789@gmail.com>
2020-08-16 23:56:44 +0530

    ADDED: Code to calculate employee monthly wage to the maximum hour in a month

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -4,20 +4,23 @@
 using namespace std;
 
 int main() {
-
 	const int EMP_RATE_PER_HOUR = 20;
 	const int NUM_OF_WORKING_DAYS = 20;
+	const int MAX_HOUR_IN_MONTH = 100;
 	const int PART_TIME = 1;
 	const int FULL_TIME = 2;
 
 	int empHrs, empWage = 0;
 	int monthlyWage = 0;
+	int totalEmpHrs = 0;
+	int totalWorkingDays = 0;
 
 	cout << "Welcome to employee wage computation" << endl;
 
 	srand(time(0));
-	for(int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-	int empCheck = rand() % 3;
+	while(totalEmpHrs <= MAX_HOUR_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = rand() % 3;
 
 		switch(empCheck) {
 			case PART_TIME:
@@ -29,10 +32,11 @@ int main() {
 			default:
 				empHrs = 0;
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		monthlyWage += empWage;
+		totalEmpHrs += empHrs;
 	}
+	monthlyWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 	cout << "Employee Monthly Wage Is: " << monthlyWage << endl;
 
 	return 0;
 }
+

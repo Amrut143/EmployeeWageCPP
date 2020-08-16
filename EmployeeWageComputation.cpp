0834ef7fffcdb4254f814d998e31cac69bb65b3f
Amrut <amrutpanda7789@gmail.com>
2020-08-16 23:26:51 +0530

    ADDED: Code to add part time employee and calculate daily wage

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -5,25 +5,30 @@ using namespace std;
 
 int main() {
 
-	int EMP_RATE_PER_HOUR = 20;
+	const int EMP_RATE_PER_HOUR = 20;
+	const int PART_TIME = 1;
+	const int FULL_TIME = 2;
 
 	int empHrs, empWage = 0;
 
 	cout << "Welcome to employee wage computation" << endl;
 
 	srand(time(0));
-	int empCheck = rand() % 2;
+	int empCheck = rand() % 3;
 
-	if(empCheck == 1) {
-		cout << "Employee is present" << endl;
-		empHrs = 8;
-	}
-	else {
-		cout << "Employee is abscent" << endl;
-		empHrs = 0;
+	switch(empCheck) {
+		case PART_TIME:
+			empHrs = 4;
+			break;
+		case FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
 	}
 	empWage = empHrs * EMP_RATE_PER_HOUR;
 	cout << "Employee Daily Wage Is: " << empWage << endl;
 
 	return 0;
 }
+

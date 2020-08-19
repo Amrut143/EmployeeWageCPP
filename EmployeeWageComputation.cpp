424309aedb96e190bc2d2d5b57305fcdf1ab4562
Amrut <amrutpanda7789@gmail.com>
2020-08-19 23:36:03 +0530

    ADDED: Code to handle multiple employee and compute their wagesg

--- EmployeeWageComputation.cpp
+++ EmployeeWageComputation.cpp
@@ -2,9 +2,31 @@
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <unistd.h>
 using namespace std;
 
-int main() {
+struct Employee {
+
+	string empName;
+	int monthlyWage;
+};
+
+void saveWageIntoFile(int monthlyWage, string name) {
+
+	fstream fileStream;
+	fileStream.open("EmpWage.txt", ios::out | ios::app);
+
+	if(fileStream.is_open()) {
+		fileStream << monthlyWage << "," << name << endl;
+		fileStream.close();
+	}
+}
+
+void computeEmpWage(string name) {
+
+	struct Employee emp;
+	emp.empName = name;
+
 	const int EMP_RATE_PER_HOUR = 20;
 	const int NUM_OF_WORKING_DAYS = 20;
 	const int MAX_HOUR_IN_MONTH = 100;
@@ -12,7 +34,6 @@ int main() {
 	const int FULL_TIME = 2;
 
 	int empHrs, empWage = 0;
-	int monthlyWage = 0;
 	int totalEmpHrs = 0;
 	int totalWorkingDays = 0;
 
@@ -35,26 +56,18 @@ int main() {
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
 		totalEmpHrs += empHrs;
-
-		fstream fileStream;
-		fileStream.open("EmpWage.txt", ios::out | ios::app);
-
-		if(fileStream.is_open()) {
-			fileStream << "Day: " << totalWorkingDays << "--->" << empWage << endl;
-			fileStream.close();
-		}
 	}
-	monthlyWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 
-	fstream fileStream;
-	fileStream.open("EmpWage.txt", ios::out | ios::app);
+	emp.monthlyWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+	cout << "Employee Monthly Wage Is: " << emp.monthlyWage << endl;
 
-	if(fileStream.is_open()) {
-		fileStream << "Employee Monthly Wage Is: " << monthlyWage << endl;
-		fileStream.close();
-	}
-	cout << "Employee Monthly Wage Is: " << monthlyWage << endl;
+	saveWageIntoFile(emp.monthlyWage, name);
+}
 
-	return 0;
+int main() {
+
+	computeEmpWage("Deepak");
+	sleep(2);
+	computeEmpWage("Dibya");
 }
 
